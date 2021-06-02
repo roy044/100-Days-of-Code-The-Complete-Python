288585d95498671ca25b6fa4ebea07689ace477e
ARINDAM ROY <arindamroy044@gmail.com>
2021-06-02 08:53:23 +0530

    Create data-types.py

---
+++ Day2/data-types.py
@@ -0,0 +1,15 @@
+
+two_digit_number = input("Type a two digit number: ")
+
+
+
+print(type(two_digit_number))
+
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+
+two_digit_number = first_digit + second_digit
+
+print(two_digit_number)
