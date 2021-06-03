e34e56552bd77950653fdea2525500fcc48e965b
ARINDAM ROY <arindamroy044@gmail.com>
2021-06-03 12:33:59 +0530

    Update banker-roulette.py

--- Day 4/banker-roulette.py
+++ Day 4/banker-roulette.py
@@ -1,14 +1,16 @@
 import random
 
-test_seed = int(input("Create a seed number: "))
-random.see(test_seed)
+# Split string method
+names_string = input("Give me everybody's names, seperated by a comma. ")
+names = names_string.split(", ")
 
-namesAsCSV = input("Give me everybody's names, seperated by a comman. ")
-names = namesAsCSV.split(",")
+#Write your code below this line 👇
 
+#Get the total number of items in list.
 num_items = len(names)
+#Generate random numbers between 0 and the last index. 
 random_choice = random.randint(0, num_items - 1)
-
+#Pick out random person from list of names using the random number.
 person_who_will_pay = names[random_choice]
 
-print(person_who_will_pay + "is going to by the meal today!")
+print(person_who_will_pay + " is going to buy the meal today!")
