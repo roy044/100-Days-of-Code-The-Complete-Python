bf4fb3f418ed664c0a0455b3e28b2b6d5bb82229
ARINDAM ROY <arindamroy044@gmail.com>
2021-06-03 10:32:53 +0530

    Update highest-score.py

--- Day 5/highest-score.py
+++ Day 5/highest-score.py
@@ -8,4 +8,4 @@ for score in student_scores:
     if score > highest_score:
         highest_score = score
 
-print(f"The highest score in the class is: {highest_score")
+print(f"The highest score in the class is: {highest_score}")
