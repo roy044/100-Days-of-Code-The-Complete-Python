3e207cc611a506937e1d6364930495105493f7c0
ARINDAM ROY <arindamroy044@gmail.com>
2021-06-03 12:31:21 +0530

    Update random-ex.py

--- Day 4/random-ex.py
+++ Day 4/random-ex.py
@@ -1,10 +1,7 @@
 import random
 
-test_seed = int(input("Create a seed number: "))
-random.seed(test_seed)
-
-randomSide = random.randint(0, 1)
-if randomSide == 1:
+random_side = random.randint(0, 1)
+if random_side == 1:
   print("Heads")
 else:
   print("Tails")
